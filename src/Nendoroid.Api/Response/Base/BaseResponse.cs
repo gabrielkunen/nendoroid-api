32f338a4c6283a9c64b722764818b183d9009c9f
Gabriel Mariano Kunen <gabriel.marianok@gmail.com>
2023-12-17 10:21:06 -0300

    remoção status code do body de retorno

--- src/Nendoroid.Api/Response/Base/BaseResponse.cs
+++ src/Nendoroid.Api/Response/Base/BaseResponse.cs
@@ -4,13 +4,11 @@ using NendoroidApi.Extensions;
 namespace NendoroidApi.Response.Base;
 
 public abstract class BaseResponse {
-    public HttpStatusCode StatusCode { get; private set; } = HttpStatusCode.OK;
     public bool Sucesso { get; private set; } = false;
     public string Mensagem { get; private set; } = "";
     public DateTime Timestamp { get; private set; }
     public BaseResponse(HttpStatusCode statusCode, string mensagem)
     {
-        StatusCode = statusCode;
         Sucesso = statusCode.IsSuccess();
         Mensagem = mensagem;
         Timestamp = DateTime.UtcNow;
